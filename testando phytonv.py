588956316fba4fa04b8eaa914975c92f46d2681d
519715 <de.deus@escola.pr.gov.br>
2025-09-24 10:43:58 -0300

    testando phyton

---
+++ testando phytonv.py
@@ -0,0 +1,15 @@
+print("Vitória de Deus 16 anos")
+print(5+7)
+print(3.14159)
+nome="Vitória de Deus"
+print("meu nome é",nome)
+pi=3.14
+print(type(pi))
+Pontuação_vôlei=200
+print(type(Pontuação_vôlei))
+data="25/04/2000"
+print(type(data))
+print(15+15)
+Resultado=10/3
+print(Resultado)
+print(type(Resultado))
